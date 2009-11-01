941bb5a16fef797dd72135fac78d961e5ce8f4d6
Zachary Stevens <zts@cryptocracy.com>
2009-11-01 11:29:39 +0000

    Move nuts and bolts of git access out of the Project class.

--- lib/Gitalist/Git/Util.pm
+++ lib/Gitalist/Git/Util.pm
@@ -1,8 +1,12 @@
 use MooseX::Declare;
 
 class Gitalist::Git::Util {
-    has git      => ( isa => NonEmptySimpleStr, is => 'ro', lazy_build => 1 );
-    sub _build_git {
+    use File::Which;
+    use Git::PurePerl;
+    use MooseX::Types::Common::String qw/NonEmptySimpleStr/;
+    has gitdir => ( isa => "Path::Class::Dir", is => 'ro' );
+    has _git      => ( isa => NonEmptySimpleStr, is => 'ro', lazy_build => 1 );
+    sub _build__git {
         my $git = File::Which::which('git');
 
         if (!$git) {
@@ -15,7 +19,25 @@ EOR
         return $git;
     }
 
-    
+    has _gpp      => ( isa => 'Git::PurePerl',   is => 'rw', lazy_build => 1 );
+    method _build__gpp {
+        my $gpp = Git::PurePerl->new(gitdir => $self->gitdir);
+        return $gpp;
+    }
+
+    method run_cmd (@args) {
+        unshift @args, ( '--git-dir' => $self->gitdir );
+        print STDERR 'RUNNING: ', $self->_git, qq[ @args], $/;
+        
+        open my $fh, '-|', $self->_git, @args
+            or die "failed to run git command";
+        binmode $fh, ':encoding(UTF-8)';
+
+        my $output = do { local $/ = undef; <$fh> };
+        close $fh;
+
+        return $output;
+    }
 
 
 
--- t/git/project.t
+++ t/git/project.t
@@ -14,8 +14,6 @@ my $proj = Gitalist::Git::Project->new(
 );
 isa_ok($proj, 'Gitalist::Git::Project');
 
-like( $proj->_git, qr#/git$#, 'git binary found');
-isa_ok($proj->_gpp, 'Git::PurePerl', 'gpp instance created');
 like($proj->path, qr#/repositories/repo1#, 'repository path is set');
 is($proj->name, qw/repo1/, 'repository name is set');
 is($proj->description, qq/some test repository/, 'repository description loaded');
