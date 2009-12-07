4866fca1226f58e934c830b9f2a1fa0861458a23
Tomas Doran <bobtfish@bobtfish.net>
2009-12-07 21:38:32 +0000

    This appears to stop the gitalist on goatse assploading

--- lib/Gitalist/Git/Repo.pm
+++ lib/Gitalist/Git/Repo.pm
@@ -67,6 +67,8 @@ name.
 
     method project (NonEmptySimpleStr $project) {
         my $path = $self->repo_dir->subdir($project)->resolve;
+        $self->repo_dir->resolve; # FIXME - This needs to be called, or if repo_dir contains .., it'll explode below!
+                                  #         This is a Path::Class::Dir bug, right?
         die "Directory traversal prohibited" unless $self->repo_dir->contains($path);
         die "Not a valid Project" unless $self->_is_git_repo($path);
         return Project->new( $self->repo_dir->subdir($project) );
