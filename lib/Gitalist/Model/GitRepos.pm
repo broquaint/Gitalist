bd7cb7a1795c0e9f54bbb3dde173ec9a10aab9f5
Tomas Doran <bobtfish@bobtfish.net>
2009-12-15 21:42:25 +0000

    And support a list of directories in the Catalyst model as config
    This is currently untested, but it is what I was aiming for, so if other
    people are ok with this then I'll rename ::GitRepos to be in line, add docs
    etc etc.

--- lib/Gitalist/Model/GitRepos.pm
+++ lib/Gitalist/Model/GitRepos.pm
@@ -2,6 +2,8 @@ package Gitalist::Model::GitRepos;
 
 use Moose;
 use Gitalist::Git::Repo;
+use Gitalist::Git::CollectionOfProjects::FromListOfDirectories;
+use MooseX::Types::Moose qw/Maybe ArrayRef/;
 use MooseX::Types::Common::String qw/NonEmptySimpleStr/;
 use Moose::Util::TypeConstraints;
 use namespace::autoclean;
@@ -22,9 +24,19 @@ has config_repo_dir => (
 );
 
 has repo_dir => (
-    isa => $repo_dir_t,
+    isa => Maybe[$repo_dir_t],
     is => 'ro',
     lazy_build => 1
+);
+
+has repos => (
+    isa => ArrayRef[$repo_dir_t],
+    is => 'ro',
+    default => sub { [] },
+    traits => ['Array'],
+    handles => {
+        _repos_count => 'count',
+    },
 );
 
 sub _build_repo_dir {
@@ -38,14 +50,19 @@ sub _build_repo_dir {
 
 after BUILD => sub {
     my $self = shift;
-    $self->repo_dir; # Explode loudly at app startup time if there is no repos
-                     # dir, rather than on first hit
+    # Explode loudly at app startup time if there is no list of
+    # projects or repos dir, rather than on first hit
+    $self->_repos_count || $self->repo_dir;
 };
 
 sub build_per_context_instance {
     my ($self, $app) = @_;
-
-    Gitalist::Git::Repo->new(repo_dir => $self->repo_dir);
+    if ($self->_repos_count) {
+        Gitalist::Git::CollectionOfProjects::FromListOfDirectories->new(repos => $self->repos);
+    }
+    else {
+        Gitalist::Git::Repo->new(repo_dir => $self->repo_dir);
+    }
 }
 
 __PACKAGE__->meta->make_immutable;
