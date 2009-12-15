7aa31dbb63f28bfcd06f05a12032153437aec5e6
Tomas Doran <bobtfish@bobtfish.net>
2009-12-15 20:55:44 +0000

    Change HasUtils to an MX::Declare class

--- lib/Gitalist/Git/HasUtils.pm
+++ lib/Gitalist/Git/HasUtils.pm
@@ -1,27 +1,27 @@
-package Gitalist::Git::HasUtils;
-use Moose::Role;
+use MooseX::Declare;
 use Gitalist::Git::Util;
-use namespace::autoclean;
 
-sub BUILD {}
-after BUILD => sub {
-    my $self = shift;
-    $self->meta->get_attribute('_util')->get_read_method_ref->($self); # Force value build.
-};
+role Gitalist::Git::HasUtils {
 
-has _util => ( isa => 'Gitalist::Git::Util',
-               lazy => 1,
-               is => 'bare',
-               builder => '_build_util',
-               handles => [ 'run_cmd',
-                            'run_cmd_fh',
-                            'run_cmd_list',
-                            'get_gpp_object',
-                            'gpp',
-                        ],
-           );
+    mehod BUILD { }
+    after BUILD {
+        $self->meta->get_attribute('_util')->get_read_method_ref->($self); # Force value build.
+    }
 
-sub _build_util { confess(shift() . " cannot build _util") }
+    has _util => ( isa => 'Gitalist::Git::Util',
+                   lazy => 1,
+                   is => 'bare',
+                   builder => '_build_util',
+                   handles => [ 'run_cmd',
+                                'run_cmd_fh',
+                                'run_cmd_list',
+                                'get_gpp_object',
+                                'gpp',
+                            ],
+               );
+
+    sub _build_util { confess(shift() . " cannot build _util") }
+}
 
 1;
 
