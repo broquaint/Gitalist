1b45851de7241967ad8ebca5de660e37ce9d79ce
Tomas Doran <bobtfish@bobtfish.net>
2010-01-09 15:28:19 +0000

    Use the new version of FCGI, avoiding gross monkeypatching

--- lib/Gitalist/Script/FastCGI.pm
+++ lib/Gitalist/Script/FastCGI.pm
@@ -2,15 +2,12 @@ package Gitalist::Script::FastCGI;
 use Moose;
 use namespace::autoclean;
 
+sub BUILD {
+    require FCGI; # Make IPC::Run happy
+    FCGI->VERSION(0.68);
+}
+
 extends 'Catalyst::Script::FastCGI';
 with 'Gitalist::ScriptRole';
-
-# Only exists so that this horrible hack can happen..
-# This should be in FCGI.pm, see:
-# http://github.com/broquaint/Gitalist/issues#issue/9
-# http://rt.cpan.org/Public/Bug/Display.html?id=50972
-# http://goatse.co.uk/~bobtfish/Gitalist/script/gitalist.fcgi/commitdiff?p=FCGI;h=6bfbe42bbc9a29f4befee56d6dd7077922cae50e
-use FCGI;
-sub FCGI::Stream::FILENO { -2 }
 
 __PACKAGE__->meta->make_immutable;
