e56c039fcdaee2e52b3bddb588f94c43cc9c86c3
Tomas Doran <bobtfish@bobtfish.net>
2010-01-10 22:41:49 +0000

    Add docs to ScriptRole

--- lib/Gitalist/ScriptRole.pm
+++ lib/Gitalist/ScriptRole.pm
@@ -22,3 +22,21 @@ before 'run' => sub {
 
 1;
 
+=head1 NAME
+
+Gitalist::ScriptRole - Role for Gitalist scripts.
+
+=head1 DESCRIPTION
+
+Wraps the run method in Catalyst scripts to apply the C<< --repo_dir >>
+option.
+
+=head1 AUTHORS
+
+See L<Gitalist> for authors.
+
+=head1 LICENSE
+
+See L<Gitalist> for the license.
+
+=cut
